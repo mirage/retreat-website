ed9dec7efce26c59bd7f3c6d07a726dfe5ca0145
Hannes Mehnert <hannes@mehnert.org>
2024-10-11 12:57:37 +0200

    adapt to mirage 4.8

--- config.ml
+++ config.ml
@@ -1,4 +1,4 @@
-(* mirage >= 4.7.0 & < 4.8.0 *)
+(* mirage >= 4.8.0 & < 4.9.0 *)
 open Mirage
 
 (* uTCP *)
@@ -26,9 +26,7 @@ let net ?group name netif =
   let i6 = create_ipv6 ?group netif ethernet in
   let i4i6 = create_ipv4v6 ?group i4 i6 in
   let tcpv4v6 = direct_tcpv4v6 name i4i6 in
-  let ipv4_only = Runtime_arg.ipv4_only ?group () in
-  let ipv6_only = Runtime_arg.ipv6_only ?group () in
-  direct_stackv4v6 ~tcp:tcpv4v6 ~ipv4_only ~ipv6_only netif ethernet arp i4 i6
+  direct_stackv4v6 ?group ~tcp:tcpv4v6 netif ethernet arp i4 i6
 
 let net = net "service" default_network
 
@@ -106,7 +104,6 @@ let () =
     optional_monitoring default_time default_posix_clock management_stack;
 
     main
-      ~runtime_args:[ runtime_arg ~pos:__POS__ "Unikernel.K.setup" ]
       ~packages
       "Unikernel.Main"
     (random @-> time @-> pclock @-> stackv4v6 @-> stackv4v6 @-> job)
--- unikernel.ml
+++ unikernel.ml
@@ -8,54 +8,42 @@ module K = struct
 
   let dns_key =
     let doc = Arg.info ~doc:"nsupdate key (name:type:value,...)" ["dns-key"] in
-    Arg.(value & opt (some key) None doc)
+    Mirage_runtime.register_arg Arg.(value & opt (some key) None doc)
 
   let dns_server =
     let doc = Arg.info ~doc:"dns server IP" ["dns-server"] in
-    Arg.(value & opt (some Mirage_runtime_network.Arg.ip_address) None doc)
+    Mirage_runtime.register_arg
+      Arg.(value & opt (some Mirage_runtime_network.Arg.ip_address) None doc)
 
   let dns_port =
     let doc = Arg.info ~doc:"dns server port" ["dns-port"] in
-    Arg.(value & opt int 53 doc)
+    Mirage_runtime.register_arg Arg.(value & opt int 53 doc)
 
   let key =
     let doc = Arg.info ~doc:"certificate key (<type>:seed or b64)" ["key"] in
-    Arg.(value & opt (some string) None doc)
+    Mirage_runtime.register_arg Arg.(value & opt (some string) None doc)
+
+  let hostname_c =
+    Arg.conv
+      ((fun s -> Result.bind (Domain_name.of_string s) Domain_name.host),
+       Domain_name.pp)
 
   let hostname =
     let doc = Arg.info ~doc:"Name of the unikernel" ["name"] in
-    Arg.(value & opt string "retreat.mirage.io" doc)
+    Mirage_runtime.register_arg
+      Arg.(value & opt hostname_c Domain_name.(host_exn (of_string_exn "retreat.mirageos.org")) doc)
 
   let no_tls =
     let doc = Arg.info ~doc:"Disable TLS" [ "no-tls" ] in
-    Arg.(value & flag doc)
+    Mirage_runtime.register_arg Arg.(value & flag doc)
 
   let http_port =
     let doc = Arg.info ~doc:"Listening HTTP port." ["http"] ~docv:"PORT" in
-    Arg.(value & opt int 80 doc)
+    Mirage_runtime.register_arg Arg.(value & opt int 80 doc)
 
   let https_port =
     let doc = Arg.info ~doc:"Listening HTTPS port." ["https"] ~docv:"PORT" in
-    Arg.(value & opt int 443 doc)
-
-  type t = {
-    dns_key : ([`raw] Domain_name.t * Dns.Dnskey.t) option;
-    dns_server : Ipaddr.t option;
-    dns_port : int;
-    key : string option;
-    no_tls : bool;
-    hostname : string;
-    http_port : int;
-    https_port : int;
-  }
-
-  let v dns_key dns_server dns_port key no_tls hostname http_port https_port =
-    { dns_key; dns_server; dns_port; key; no_tls; hostname; http_port; https_port }
-
-  let setup =
-    Term.(const v $ dns_key $ dns_server $ dns_port $ key $ no_tls
-          $ hostname $ http_port $ https_port)
-
+    Mirage_runtime.register_arg Arg.(value & opt int 443 doc)
 end
 
 module Main (R : Mirage_crypto_rng_mirage.S) (T : Mirage_time.S) (P : Mirage_clock.PCLOCK) (S : Tcpip.Stack.V4V6) (Management : Tcpip.Stack.V4V6) = struct
@@ -101,14 +89,14 @@ module Main (R : Mirage_crypto_rng_mirage.S) (T : Mirage_time.S) (P : Mirage_clo
       Logs.warn (fun m -> m "TLS error %a" TLS.pp_write_error e);
       S.TCP.close tcp_flow
 
-  let start _random _time _pclock stack management arg =
-    let hostname = Domain_name.(host_exn (of_string_exn arg.K.hostname)) in
+  let start _random _time _pclock stack management =
+    let hostname = K.hostname () in
     let data =
       let content_size = Cstruct.length Page.rendered in
       [ header content_size ; Page.rendered ]
     in
-    (if not arg.no_tls then
-       match arg.dns_key, arg.dns_server, arg.key with
+    (if not (K.no_tls ()) then
+       match K.dns_key (), K.dns_server (), K.key () with
        | None, _, _ | _, None, _ | _, _, None ->
          Logs.err (fun m -> m "TLS operations requires dns-key, dns-server, and key arguments");
          exit Mirage_runtime.argument_error
@@ -129,7 +117,7 @@ module Main (R : Mirage_crypto_rng_mirage.S) (T : Mirage_time.S) (P : Mirage_clo
          Dns_certify.retrieve_certificate
            stack ~dns_key:(Fmt.to_to_string Dns.Dnskey.pp_name_key dns_key)
            ~hostname ~key_type ?key_data ?key_seed
-           dns_server arg.dns_port >|= function
+           dns_server (K.dns_port ()) >|= function
          | Error (`Msg msg) ->
            Logs.err (fun m -> m "error while requesting certificate: %s" msg);
            exit Mirage_runtime.argument_error
@@ -140,11 +128,11 @@ module Main (R : Mirage_crypto_rng_mirage.S) (T : Mirage_time.S) (P : Mirage_clo
              Logs.err (fun m -> m "error while building TLS configuration: %s" msg);
              exit Mirage_runtime.argument_error
            | Ok tls_config ->
-             Logs.info (fun m -> m "listening for HTTPS on port %u" arg.https_port);
-             S.TCP.listen (S.tcp stack) ~port:arg.https_port (serve_tls tls_config data)
+             Logs.info (fun m -> m "listening for HTTPS on port %u" (K.https_port ()));
+             S.TCP.listen (S.tcp stack) ~port:(K.https_port ()) (serve_tls tls_config data)
      else
        Lwt.return_unit) >>= fun () ->
-    Logs.info (fun m -> m "listening for HTTP in port %u" arg.http_port);
-    S.TCP.listen (S.tcp stack) ~port:arg.http_port (serve data) ;
+    Logs.info (fun m -> m "listening for HTTP in port %u" (K.http_port ()));
+    S.TCP.listen (S.tcp stack) ~port:(K.http_port ()) (serve data) ;
     S.listen stack
 end
